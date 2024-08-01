53576c14c33696dee80224262e69219d246666f3
Martin Brandhaug <martin@brandhaug.net>
2024-08-01 10:35:22 +0200

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/**
+ * https://www.notion.so/strise/How-to-git-Strise-0469f1d792b44e38b31c8f54657b3087#ce265e59e3914336956643d2c341bb02
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
